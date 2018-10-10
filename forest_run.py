7e76f5aa65ac76aafe380317ac1fdd04c7220dd7
raed1337 <mohamedraed.elaoun@gmail.com>
2018-10-10 10:23:13 +0100

    data vizualization

--- forest_run.py
+++ forest_run.py
@@ -4,7 +4,8 @@ plt.style.use('ggplot')
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 
 url = "C:/Users/kirito/Desktop/train.csv"
@@ -39,3 +40,17 @@ plt.xlabel('component 1')
 plt.ylabel('component 2')
 plt.colorbar();
 plt.show()
+
+url_test = "C:/Users/kirito/Desktop/test.csv"
+
+test_x = pd.read_csv(url_test)
+print(test_x.head(100))
+
+X_train, X_Cross_Validation, y_train, y_Cross_Validation = train_test_split(train_x,train_y,test_size=0.2,random_state=4)
+
+lr=LogisticRegression(solver='lbfgs')
+lr.fit(X_train , y_train['Cover_Type'])
+lr.predict(X_Cross_Validation)
+score = lr.score(X_Cross_Validation,y_Cross_Validation['Cover_Type'])
+print(pd.DataFrame(lr.predict_proba(X_Cross_Validation)))
+
